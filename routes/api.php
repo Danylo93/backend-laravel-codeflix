245ab6a7252f9e9c852560c3095ac8a863d68fb4
Danylo Oliveira <danylo.oliveira73@gmail.com>
2023-12-07 20:02:53 -0300

    finalizacão tests de e2e e api genres

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,12 @@
 
 use App\Http\Controllers\Api\{
     CategoryController,
+    GenreController
 };
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('/categories', CategoryController::class);
+Route::apiResource('/genres', GenreController::class);
 
 Route::get('/', function () {
     return response()->json(['message' => 'success']);
