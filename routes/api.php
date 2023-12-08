264d6776a3d4ccc77a7438ec6569ede49b0c753d
Danylo Oliveira <danylo.oliveira73@gmail.com>
2023-12-07 21:45:48 -0300

    finalizacão e2e e api castmember

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,15 @@
 <?php
 
 use App\Http\Controllers\Api\{
+    CastMemberController,
     CategoryController,
     GenreController
 };
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('/categories', CategoryController::class);
-Route::apiResource('/genres', GenreController::class);
+Route::apiResource(name: '/genres', controller: GenreController::class);
+Route::apiResource('/cast_members', CastMemberController::class);
 
 Route::get('/', function () {
     return response()->json(['message' => 'success']);
